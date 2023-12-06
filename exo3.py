299101d30b786cbb1adde462bcb92caf9a39f184
AbdiLouay <ablouay5@gmail.com>
2023-12-06 15:18:23 +0100

    3.2

--- exo3.py
+++ exo3.py
@@ -1,23 +1,18 @@
-from math import*
-
-def display_solution(x1, x2):
-    return "Les solutions sont: " + str(x1) + " et " + str(x2)
-
+from math import *
 def resol_second_degre (a,b,c):
+
     if a !=0:
         delta = b**2-4*a*c
 
         if delta < 0:
-            return "il à pas solution réelle"
+            return ("il à pas solution réelle")
         elif delta == 0:
             x = -b / (2*a)
-            return display_solution(x, x)
+            return ("il y a une solution réelle" + str(x))
         else:
             x1 = (-b-sqrt(delta)) / (2*a)
             x2 = (-b+sqrt(delta)) / (2*a)
-            return display_solution(x1, x2)
-    else:
-        return "la première coordonnée n'est pas égale à 0"
-
-print(resol_second_degre(1,-3,2))
-print(resol_second_degre(0,-3,2))
+         return ("il y a deux solution réelle" + str(x1) + "et" + str(x2))
+    else;
+        return("a=0. ce n'est pas uen fonction du second degré")
+        
